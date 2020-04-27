e0686bc5ba25ad944e7dc4bf885b69defb386e5f
Constantine Stoyanov <1039pm@gmail.com>
2020-04-27 16:36:48 +0300

    kafka config - use map instead of properties; store all configs in kafka producer properties component;

--- kprod/src/main/java/com/github/coobik/kprod/kafka/KafkaProducerConfiguration.java
+++ kprod/src/main/java/com/github/coobik/kprod/kafka/KafkaProducerConfiguration.java
@@ -1,13 +1,12 @@
 package com.github.coobik.kprod.kafka;
 
-import java.util.Properties;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.support.serializer.JsonSerializer;
@@ -18,38 +17,41 @@ import com.github.coobik.kprod.model.Message;
 @Configuration
 public class KafkaProducerConfiguration {
 
-  // TODO: kafka producer properties bean
-  @Value("${kafka.bootstrap.servers}")
-  private String kafkaBootstrapServers;
-
-  @Value("${kafka.producer.clientId}")
-  private String clientId;
+  @Autowired
+  private KafkaProducerProperties kafkaProducerProperties;
 
   // TODO: spring boot kafka template
 
   @Autowired
   @Bean("streamKafkaProducer")
-  public KafkaProducer<String, Message> streamKafkaProducer(
-      @Qualifier("streamKafkaProducerProperties") Properties streamKafkaProducerProperties) {
-    return new KafkaProducer<String, Message>(streamKafkaProducerProperties);
+  public KafkaProducer<String, Message> streamKafkaProducer() {
+    return new KafkaProducer<String, Message>(streamKafkaProducerConfig());
   }
 
-  @Bean("streamKafkaProducerProperties")
-  public Properties streamKafkaProducerProperties() {
-    Properties properties = new Properties();
+  /**
+   * config map for kafka producer. do not make this map a Bean because spring wraps it with another
+   * map
+   * 
+   * @see <a href=
+   *      "https://stackoverflow.com/questions/41056765/spring-java-config-wraps-injected-map">stackoverflow</a>
+   */
+  private Map<String, Object> streamKafkaProducerConfig() {
+    Map<String, Object> config = new HashMap<>();
 
-    properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaBootstrapServers);
-    properties.put(ProducerConfig.CLIENT_ID_CONFIG, clientId);
-    properties.put(ProducerConfig.ACKS_CONFIG, "all");
-    properties.put(ProducerConfig.RETRIES_CONFIG, 3);
-    properties.put(ProducerConfig.RETRY_BACKOFF_MS_CONFIG, 1000);
-    properties.put(ProducerConfig.BATCH_SIZE_CONFIG, 0);
-    properties.put(ProducerConfig.LINGER_MS_CONFIG, 0);
-    // properties.put(ProducerConfig.BUFFER_MEMORY_CONFIG, 33554432); // 32 mb
-    properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-    properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+    config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,
+        kafkaProducerProperties.getBootstrapServers());
 
-    return properties;
+    config.put(ProducerConfig.CLIENT_ID_CONFIG, kafkaProducerProperties.getClientId());
+    config.put(ProducerConfig.ACKS_CONFIG, "all");
+    config.put(ProducerConfig.RETRIES_CONFIG, kafkaProducerProperties.getRetries());
+    config.put(ProducerConfig.RETRY_BACKOFF_MS_CONFIG, kafkaProducerProperties.getRetryBackoffMs());
+    config.put(ProducerConfig.BATCH_SIZE_CONFIG, 0);
+    config.put(ProducerConfig.LINGER_MS_CONFIG, 0);
+    // config.put(ProducerConfig.BUFFER_MEMORY_CONFIG, 33554432); // 32 mb
+    config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+    config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+
+    return config;
   }
 
 }
