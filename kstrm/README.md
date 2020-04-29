5605ebf2ddafb81a1979d6da6cfe7786f199ce74
Constantine Stoyanov <1039pm@gmail.com>
2020-04-29 12:42:14 +0300

    kstrm - init - spring boot 2.1.13.RELEASE

---
+++ kstrm/README.md
@@ -0,0 +1,13 @@
+# kstrm
+
+Example Kafka Streams built on Spring Boot 2.1.13.RELEASE
+
+### Message
+
+- **value** - random alphanumeric string of `report.value.length` chars
+- **unit** - string
+- **timestamp** - current time in millis (long)
+
+## Settings
+
+- `server.port`
---
+++ kstrm/src/test/java/com/github/coobik/kstrm/KstrmApplicationTests.java
@@ -0,0 +1,17 @@
+package com.github.coobik.kstrm;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KstrmApplicationTests {
+
+  @Test
+  public void contextLoads() {
+  }
+
+}
